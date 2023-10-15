e394172974298736fdb0948e1cbad12199ebb8f7
Patrick Millerson <patmillerson@gmail.com>
2023-10-14 23:53:11 -0400

    some22

--- exsampla_app/settings.py
+++ exsampla_app/settings.py
@@ -1,5 +1,8 @@
 import os
 from pathlib import Path
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -111,3 +114,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+print(env('TEST'))
